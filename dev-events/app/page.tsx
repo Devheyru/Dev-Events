45c30692d863d37c9c00b49506692a3605c42cdb
Heyru Jemal <heyru638@gmail.com>
2025-11-01 17:25:16 +0300

    upper landing page

--- dev-events/app/page.tsx
+++ dev-events/app/page.tsx
@@ -1,7 +1,19 @@
+import ExploreBtn from "@/components/ExploreBtn";
 import React from "react";
 
 const page = () => {
-  return <div>Dev Events Page</div>;
+  return (
+    <section>
+      <h1 className="text-center">
+        The Hub for Every Dev Event <br /> you can&apos;t miss out{" "}
+      </h1>
+      <p className="text-center mt-5">
+        Hackathons, Meetups and Conferences all in one place
+      </p>
+
+      <ExploreBtn />
+    </section>
+  );
 };
 
 export default page;
